3c289128f22abfeac23542abfa6d49d1be228014
Sanojkr4120 <sanoj2003feb11@gmail.com>
2026-01-10 12:32:15 +0530

    update

--- CineSineV1/frontend/src/components/Navbar.jsx
+++ CineSineV1/frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className={`fixed top-0 w-full z-50 transition-all duration-500 ease-in-out ${isScrolled || !isHome ? ' py-4 shadow-sm' : 'bg-transparent py-6 md:py-8'} px-6 md:px-16 flex justify-between items-center`}>
+        <nav className={`fixed top-0 w-full z-50 transition-all duration-500 ease-in-out ${isScrolled || !isHome ? 'bg-[#f8f4ed] py-4 shadow-sm' : 'bg-transparent py-6 md:py-8'} px-6 md:px-16 flex justify-between items-center`}>
 
             {/* Logo */}
 
